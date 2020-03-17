469fa4f485eceb0a41e658e55720b5568af88262
atomjaylee <908105576@qq.com>
2020-03-18 07:34:03 +0800

    #fix pub publish success

--- lib/worker_utils.dart
+++ lib/worker_utils.dart
@@ -1,7 +1,7 @@
 library worker_utils;
 
 import "dart:ui";
-import './utils/convertColor.dart';
+import 'utils/convertColor.dart';
 
 class MWorker {
   static Color changeHex(String colorHex) => MColor.hex(colorHex);
